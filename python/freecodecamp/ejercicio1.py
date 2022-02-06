084ae82c1b85171e5103ee80e471f3b5bc98e114
david portatil <david@buenhombre.com>
2022-02-06 13:27:40 +0100

    mas cambios ejercicio  1

--- python/freecodecamp/ejercicio1.py
+++ python/freecodecamp/ejercicio1.py
@@ -1,7 +1,11 @@
 print ('ejercicio 1')
-numero = 2
+numero = input ('Dame un numero!!!!')
+valor = int(numero)
 print (numero)
-numero = 2+2
+numero2 = input ('Dame otro numero!!!')
+valor2 = int(numero2)
+total = numero + numero2
+valortotal = valor + valor2
 print ('el total es') 
-print (numero)
-
+print (total)
+print (valortotal)
